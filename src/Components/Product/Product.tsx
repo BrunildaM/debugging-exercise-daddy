72c27b293b75fa706e78774d4505ff3749548365
Brunilda Muco <brunildamuco6@gmail.com>
2022-10-14 16:47:28 +0200

    first commit

---
+++ src/Components/Product/Product.tsx
@@ -0,0 +1,33 @@
+
+import { Product as ProductType } from '../../utils/types'
+import useProduct from './useProduct';
+import './style.css';
+import { gK, strings } from '../../utils/strings-english';
+
+type Props = {
+  product: ProductType;
+}
+
+function Product({ product }: Props)  {
+  const { handleToggleFold, isUnfolded } = useProduct();
+
+  return (
+    <div
+      key={product.id}
+      className="product"
+      role='button'
+      onChange={() => handleToggleFold}
+    >
+      <div className="product__content">
+        <img src={product.thumbnail} alt={product.title} />
+        <p>
+          {product.title}
+        </p>
+      </div>
+
+      {isUnfolded ? <span>{product.description}</span> : <p>{gK('more')}</p>}
+    </div>
+  )
+}
+
+export default Product
---
+++ src/Components/ProductList/ProductList.tsx
@@ -0,0 +1,24 @@
+import { Product as ProductType } from "../../utils/types";
+import Product from "../Product";
+import "./style.css";
+
+type Props = {
+  products: ProductType[];
+};
+
+function ProductList({ products }: Props) {
+  if (products === null) {
+    return <h1>Loading</h1>;
+  }
+  return (
+    <div>
+      <ul>
+        {products.map((product) => (
+          <Product product={product} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ProductList;
